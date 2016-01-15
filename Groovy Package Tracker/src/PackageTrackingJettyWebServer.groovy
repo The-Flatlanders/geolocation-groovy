6fd635b6cef30fef5bbb2915f2ec7744abfb6b8c
Ari Mindell <amindell11@gmail.com>
2016-01-15 10:36:31 -0500

    Added HTML parser which parses file in HTML folder and displays it on the server/trackPackages

--- Groovy Package Tracker/src/PackageTrackingJettyWebServer.groovy
+++ Groovy Package Tracker/src/PackageTrackingJettyWebServer.groovy
@@ -4,27 +4,28 @@ import org.eclipse.jetty.servlet.*
 import groovy.json.JsonSlurper
 import javax.servlet.http.*
 import javax.servlet.*
-                        
+
 
 class SimpleGroovyServlet extends HttpServlet {
 	HashMap trackedIDs=new HashMap();
-    long updateCount = 0;
-    long lastPrintCount = 0;
-    void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        println "GET  "+req.getRequestURL()+"   query string:"+req.getQueryString();
-        def uuids = req.getParameterMap().get("uuid")
-        if(req.getPathInfo().equals("/tracknewpackage")) {			
-            def responseString = "{ \"ackUUID\":\""+uuids+"\" }"
+	long updateCount = 0;
+	long lastPrintCount = 0;
+	void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		// println "GET  "+req.getRequestURL()+"   query string:"+req.getQueryString();
+		def uuids = req.getParameterMap().get("uuid")
+		
+		if(req.getPathInfo().equals("/tracknewpackage")) {
+			def responseString = "{ \"ackUUID\":\""+uuids+"\" }"
 			double lat=Double.parseDouble(req.getParameterMap().get("destinationLat")[0])
 			double lon=Double.parseDouble(req.getParameterMap().get("destinationLon")[0])
 			trackedIDs.putAt(uuids[0],new TrackablePackage(uuids[0], new Coordinate(lat,lon)));
-            resp.setContentType("application/json");
-            def writer = resp.getWriter();
-            writer.print(responseString);
-            writer.flush();
-            println "\t\t  "+responseString;
-        }
-		else if(req.getPathInfo().equals("/trackPackages")){
+			resp.setContentType("application/json");
+			def writer = resp.getWriter();
+			writer.print(responseString);
+			writer.flush();
+			println "\t\t  "+responseString;
+		}		
+		if(req.getPathInfo().equals("/trackPackages")){
 			def packageInfos=new ArrayList()
 			for(String id:uuids){
 				if(trackedIDs.containsKey(id)){
@@ -34,55 +35,55 @@ class SimpleGroovyServlet extends HttpServlet {
 			def writer = resp.getWriter();
 			for(int x=0;x<packageInfos.size();x++){
 				writer.print(packageInfos.get(x))
+				writer.print("\n\n");
 			}
-			Scanner scanner = new Scanner( new File("HTML/TrackNewPackageForm.html") );
+			resp.setContentType("txt/html")
+			Scanner scanner = new Scanner( new File("HTML/TrackNewPackageForm.HTML") );
 			String text = scanner.useDelimiter("\\A").next();
 			scanner.close()
-			resp.setContentType("text/html");
 			writer.print(text);
 			writer.flush();
 		}
 
-    }
+	}
 
 
-    void doPost(HttpServletRequest req, HttpServletResponse resp) {
-//        println "POST: "+req.getRequestURL();
+	void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		//        println "POST: "+req.getRequestURL();
+		if(req.getPathInfo().startsWith("/packagetrackupdate/")) {
 
-        if(req.getPathInfo().startsWith("/packagetrackupdate/")) {
-
-            try {
-                BufferedReader reader = req.getReader();
-                String line = null;
-                while ((line = reader.readLine()) != null) {
+			try {
+				BufferedReader reader = req.getReader();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+				
 					def slurper=new JsonSlurper()
 					def inf=slurper.parseText(line);
 					def uuid = req.getPathInfo().replace("/packagetrackupdate/","");
-					if(trackedIDs.containsKey(uuid))trackedIDs.get(uuid).setLocation(new Coordinate(Double.parseDouble(inf.lat),Double.parseDouble(inf.lon)))
-					println(trackedIDs.get(uuid,null))
-					//lat=Double.parseDouble(line.substring(line.indexOf("lat")+6,line.indexOf("lon")-3));
-                    if(line.contains("delivered")) {
-                        println req.getPathInfo()+" -> "+line;
-                    }
-                    else {
-                        //comment out if you only want to print the delivered
-                        //events
-                        println req.getPathInfo()+" -> "+line;
-                    }
-                }
+					def currentPackage = trackedIDs.get(uuid);
+					if(line.contains("delivered")) {
+						//This code registers delivery events
+						println uuid +" -> "+ line;
+						currentPackage.setDelivered(true);
+					}
+					else{
+						//This code tracks all non delivery events
+						currentPackage.setLocation(new Coordinate(Double.parseDouble(inf.lat),Double.parseDouble(inf.lon)))
+						println req.getPathInfo()+" -> "+line; //Comment out if you only want to print the delivered updates
+					}
+				}
 
-                updateCount++;
-                if((updateCount - lastPrintCount) >= 1000) {
-                    println "packagetrackupdate count: "+updateCount;
-                    lastPrintCount = updateCount;
-                }
+				updateCount++;
+				if((updateCount - lastPrintCount) >= 1000) {
+					println "packagetrackupdate count: "+updateCount;
+					lastPrintCount = updateCount;
+				}
 
-            } catch (Exception e) { e.printStackTrace(); /*report an error*/ }
-        }
-    }
+			} catch (Exception e) { e.printStackTrace(); /*report an error*/ }
+		}
+	}
 
 }
-
 
 def server = new Server(8080);
 ServletHandler handler = new ServletHandler();
