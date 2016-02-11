f8634ed4f99bcc70800fac933403dea997aa84dd
JoshuaEng <joshuaengels@yahoo.com>
2016-02-11 14:25:26 -0500

    Documented methods and classes

--- Groovy Package Tracker/src/Coordinate.groovy
+++ Groovy Package Tracker/src/Coordinate.groovy
@@ -1,24 +1,57 @@
+/**
+ * Represents a single point located on the glode. All fields of the class are final. If the object represented
+ * by the Coordinate moves, than a new point should be constructed.
+ */
 public class Coordinate {
 	private final double lon;
 	private final double lat;
 	private final double ele;
+	
+	/**
+	 * Makes a new coordinate at a given lat, lon, and elevation
+	 * Throws an IllegalArgumentException if lat or lon is out of bounds
+	 * @param req The latitude of the point
+	 * @param resp The longitude of the point
+	 */
 	public Coordinate(double lat, double lon, double ele){
+		if(Math.abs(lon) >= 180 || Math.abs(lat) >= 90){
+			throw new IllegalArgumentException();
+		}
 		this.lat=lat;
 		this.lon=lon;
 		this.ele=ele;
 	}
+	
+	/**
+	 * Makes a new coordinate at a given lat, and lon
+	 * Throws an IllegalArgumentException if lat or lon is out of bounds
+	 * @param req The latitude of the point
+	 * @param resp The longitude of the point
+	 */
 	public Coordinate(double lat,double lon){
 		this(lat,lon,0.0);
 	}
+	
+	
 	public double getlon(){
 		return lon;
 	}
+	
 	public double getlat(){
 		return lat;
 	}
+	
 	public String toString(){
 		return "{"+lat+","+lon+","+ele+"}";
 	}
+	
+	
+	/**
+	 * Finds the distance between two Coordinate objects
+	 * @param Coord1 
+	 * @param Coord2
+	 * @return
+	 */
 	public static double getDistance(Coordinate Coord1,Coordinate Coord2){
 		final int R = 6371; // Radius of the earth
 		double lat1=Coord1.lat;
@@ -36,6 +69,13 @@ public class Coordinate {
 		distance = Math.pow(distance, 2) + Math.pow(height, 2);
 		return Math.sqrt(distance);
 	}
+	
+	/**
+	 * Finds the midpoint between two Coordinate objects
+	 * @param Coord1
+	 * @param Coord2
+	 * @return The Coordinate that is the midpoint between the two given Coordinates
+	 */
 	public static Coordinate midPoint(Coordinate Coord1,Coordinate Coord2){
 		double lat1=Coord1.lat;
 		double lat2=Coord2.lat;
@@ -51,7 +91,6 @@ public class Coordinate {
 		double By = Math.cos(lat2) * Math.sin(dLon);
 		double lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
 		double lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
-		return new Coordinate(Math.toDegrees(lat3),Math.toDegrees(lon3))
+		return new Coordinate(Math.toDegrees(lat3),Math.toDegrees(lon3));
 	}
 }
-
